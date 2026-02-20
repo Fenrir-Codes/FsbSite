a7333ff980cf6d20ff6f64fa9c9c815824892a8f
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2026-02-20 16:45:30 +0100

    Added a login page and Contact page sample

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
-import { Home} from './home/home';
+import { Home } from './home/home';
 import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Loginpage } from './loginpage/loginpage';
 
 export const routes: Routes = [
-  { path: '', component: Home }, // Default page
-  { path: 'about', component: About }, // The "Om fsb" page
+  { path: '', component: Home },             // Home page
+  { path: 'about', component: About },       // "Om fsb" page
+  { path: 'contact', component: Contact }, // Contact page
+  { path: 'login', component: Loginpage },  // to login page
+  { path: '**', redirectTo: '' }             // non existent url, returning us to landingpage Home
 ];
