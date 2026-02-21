17a4fa2ef162d34540f65a438f358b52ac351b5a
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2026-02-21 14:54:35 +0100

    added department page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { Loginpage } from './loginpage/loginpage';
+import { Department } from './department/department';
 
 export const routes: Routes = [
   { path: '', component: Home },             // Home page
   { path: 'about', component: About },       // "Om fsb" page
   { path: 'contact', component: Contact }, // Contact page
-  { path: 'login', component: Loginpage },  // to login page
+  { path: 'login', component: Loginpage },
+  { path: 'depatment', component: Department },  // to login page
   { path: '**', redirectTo: '' }             // non existent url, returning us to landingpage Home
 ];
